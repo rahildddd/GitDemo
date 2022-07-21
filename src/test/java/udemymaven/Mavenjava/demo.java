08dd12f821388e25e2c9801ff240c5422f98648c
rahul <rahuldahikar18@gmail.com>
2022-07-21 13:51:00 +0530

    fnal coommit bu asssian dev team

--- src/test/java/udemymaven/Mavenjava/demo.java
+++ src/test/java/udemymaven/Mavenjava/demo.java
@@ -1,10 +1,11 @@
 package udemymaven.Mavenjava;
 
 public class demo {
-
 	
-private void sysp() {
-	// TODO Auto-generated method stub
-System.out.println(" new class");
-}
+  public  void sysou() {
+		// TODO Auto-generated method stub
+	  System.out.println("dygsjgdjgsg");
+
+	}
+	
 }
